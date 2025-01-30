9fa3f4de33f3aefe91497526581b0efa625fb28a
Adi Gunawan Silalahi <adigunawansilalahi@gmail.com>
2025-01-30 22:03:04 +0700

    update admin layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-    <h2 class="mt-5">Admin Dashboard</h2>
-    <p>Selamat datang, {{ Auth::user()->name }}!</p>
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit" class="btn btn-danger">Logout</button>
-    </form>
-</div>
-</body>
-</html>
+<?
+@extends('main')
+@section('content')
+
+@endsection
+
+?>
+
